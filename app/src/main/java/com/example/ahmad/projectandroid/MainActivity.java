8ccfd85a3cccd2e5af282c6d7457f4ca2641b8a2
Ahmad <hum115@hotmail.com>
2016-03-14 20:37:02 -0400

    added the connection files

--- app/src/main/java/com/example/ahmad/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/ahmad/projectandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ahmad.projectandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button gotoconnection= (Button) findViewById(R.id.hrbutton);
+        gotoconnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, heartrate.class);
+                startActivity(intent);
+            }
+        });
         
     }
+
+
 
 }
